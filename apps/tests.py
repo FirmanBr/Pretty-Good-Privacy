dfcc68cd70a7f68f0ff55b4f40dc770784b91fb3
FirmanBr <firmanbrilian@gmail.com>
2019-11-29 08:04:32 +0700

    version 291120190804

--- apps/tests.py
+++ apps/tests.py
@@ -1,3 +1,23 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from apps.models import PairKeyReq
 
-# Create your tests here.
+class ModelTest(TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+
+        super(ModelTest, cls).setUpClass()
+
+        dept = PairKeyReq(fullname='Firman Brilian', email='firmanbriliant@gmail.com')
+        dept.save()
+
+        dept = PairKeyReq.objects.get(fullname='Firman Brilian')
+        print('Added key data : ')
+        print(dept)
+        print('')
+
+    def test_key_models(self):
+        dept = PairKeyReq.objects.get(fullname='Firman Brilian')
+        self.assertEqual(dept.email,'firmanbrilian@gmail.com')
+
